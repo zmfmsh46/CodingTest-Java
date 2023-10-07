fd1976789647d887e8b9852dacb62ef2840ee05a
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2023-10-08 02:06:45 +0900

    Update 문자 개수 세기.java

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -19,3 +19,17 @@ class Solution {
         return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
+
+/*class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        for(int i = 0; i < my_string.length(); i++){
+            char c = my_string.charAt(i);
+            if(c >= 'a')
+                answer[c - 'a' + 26]++;
+            else
+                answer[c - 'A']++;
+        }
+        return answer;
+    }
+}*/

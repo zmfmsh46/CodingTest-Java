9b9aeb1cc972641dcc9135db13056cac2648fae6
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2023-10-25 10:36:01 +0900

    [level unrated] Title: 뒤에서 5등까지, Time: 2.40 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -2,9 +2,10 @@ import java.util.*;
 import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int[] num_list) {
-        Arrays.sort(num_list);
-
         
-        return IntStream.rangeClosed(0,4).map(i -> num_list[i]).toArray();
+        return Arrays.stream(num_list)
+            .sorted()
+            .limit(5)
+            .toArray();
     }
 }

56ac859a417f31671b5d3912b8b279f167f425a6
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2023-10-26 11:25:24 +0900

    [level unrated] Title: 정수 찾기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        for (int num : num_list) {
+            if (num == n) {
+                answer = 1;
+            }
+        }
+        return answer;
+    }
+}

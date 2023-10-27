06eb9e8bd67688ac71040f6456b0dd8a52ab07ad
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2023-10-27 11:04:50 +0900

    [level unrated] Title: 커피 심부름, Time: 0.03 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        
+        for (String str : order) {
+            answer += str.contains("cafelatte") ? 5000 : 4500;
+        }
+        
+        return answer;
+    }
+}

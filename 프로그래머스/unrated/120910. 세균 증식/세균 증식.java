98857550378e8725c08557042d663b970965fda8
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2023-11-02 11:51:29 +0900

    [level unrated] Title: 세균 증식, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
@@ -1,6 +1,9 @@
 class Solution {
     public int solution(int n, int t) {
-        int answer = n * (int) Math.pow(2,t);
+        //int answer = n * (int) Math.pow(2,t);
+        //비트연산으로 한칸씩 밀면 2를 곱한거랑 같은거니까 t만큼 밀어주면 정답이 나옴
+        
+        int answer = n << t;
         return answer;
     }
 }

4bf7ed7963c534c787a36df7e38460388a199d8c
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2023-11-04 21:44:57 +0900

    [level unrated] Title: 약수 구하기, Time: 1.82 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
+import java.util.stream.IntStream;
+
 
 class Solution {
     public int[] solution(int n) {
-        ArrayList<Integer> answer = new ArrayList<>();
+        /*ArrayList<Integer> answer = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 answer.add(i);
             }
         }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();*/
+        
+        return IntStream.rangeClosed(1, n).filter(i -> n % i == 0).toArray();
     }
 }

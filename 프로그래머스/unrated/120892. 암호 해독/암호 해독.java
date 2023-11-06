8b7366fd4b1b8d92540eb2368a370f8431265022
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2023-11-06 11:18:33 +0900

    [level unrated] Title: 암호 해독, Time: 0.04 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = code -1; i < cipher.length(); i+=code) {
+            sb.append(cipher.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}

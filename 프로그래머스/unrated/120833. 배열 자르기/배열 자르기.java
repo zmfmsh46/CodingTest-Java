1fc3dcb66328cb270c6a5a5fdb79aa887387a437
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2023-12-04 11:56:43 +0900

    [level unrated] Title: 배열 자르기, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
@@ -2,10 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        ArrayList<Integer> answer = new ArrayList<>();
+        /*ArrayList<Integer> answer = new ArrayList<>();
         for (int i = num1; i <= num2; i++) {
             answer.add(numbers[i]);
-        }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        }*/
+        
+        //return answer.stream().mapToInt(Integer::intValue).toArray();
+        
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 }

9d3f3a9f4991fef194a950cf96b498261997f8ed
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2023-12-06 11:31:28 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -2,9 +2,12 @@ class Solution {
     public String solution(String my_string) {
         StringBuilder sb = new StringBuilder();
 
-        for (int i = my_string.length()-1; i >= 0; i--) {
+        /*for (int i = my_string.length()-1; i >= 0; i--) {
             sb.append(my_string.charAt(i));
-        }
+        }*/
+        
+        sb.append(my_string);
+        sb.reverse();
         return sb.toString();
     }
 }

ffe2803bd0bdc119b4b3880344c6c741055c1c34
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2024-02-08 10:55:07 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 69.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long number = num;
+        
+        while (number != 1) {
+            if (number % 2 == 0) {
+                number /= 2;
+                answer++;
+            } else {
+                number = number * 3 + 1;
+                answer++;
+            }
+
+            if (answer == 500) {
+                answer = -1; 
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}

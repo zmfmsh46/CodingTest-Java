b3ab6bc7157290598ac7848fc37103d3a278bc95
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2024-02-27 11:24:57 +0900

    [level 1] Title: 시저 암호, Time: 1.88 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -2,23 +2,15 @@ class Solution {
     public String solution(String s, int n) {
         String answer = "";
         StringBuilder stringBuilder = new StringBuilder();
-
+        n = n % 26;
         for (char c : s.toCharArray()) {
             if (c == ' ') {
                 stringBuilder.append(c);
             } else {
-                if (65 <= c && c <= 90) {
-                    if (c + n > 90) {
-                        stringBuilder.append((char) ((c + n) % 90 + 64));
-                    } else {
-                        stringBuilder.append((char) (c + n));
-                    }
-                } else if (97 <= c && c <= 122) {
-                    if (c + n > 122) {
-                        stringBuilder.append((char) ((c + n) % 122 + 96));
-                    } else {
-                        stringBuilder.append((char) (c + n));
-                    }
+                if (Character.isLowerCase(c)) {
+                    stringBuilder.append((char) ((c - 'a' + n) % 26 + 'a'));
+                } else if (Character.isUpperCase(c)){
+                    stringBuilder.append((char) ((c - 'A' + n) % 26 + 'A'));
                 }
             }
         }

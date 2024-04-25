144a9ffd185497f79b654ccab4f16ce1d1f97207
zmfmsh46 <105578140+zmfmsh46@users.noreply.github.com>
2024-04-25 16:11:34 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int sum = 0;
+        for (int i = 0; i < a - 1; i++) {
+            sum += month[i];
+        }
+        sum = (sum + b) % 7;
+        switch (sum) {
+            case 0:
+                answer = "THU";
+                break;
+            case 1:
+                answer = "FRI";
+                break;
+            case 2:
+                answer = "SAT";
+                break;
+            case 3:
+                answer = "SUN";
+                break;
+            case 4:
+                answer = "MON";
+                break;
+            case 5:
+                answer = "TUE";
+                break;
+            case 6:
+                answer = "WED";
+                break;
+        }
+        return answer;
+    }
+}
